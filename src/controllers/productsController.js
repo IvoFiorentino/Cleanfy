a2f0c31fb96d745fb929963684751264d8300b21
MateoDomian <mateodomian@gmail.com>
2022-02-13 22:34:28 -0300

    detail

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -43,15 +43,11 @@ const productsController = {
 
   productName: (req, res) => {
     let productId = req.params.id;
-    let promProduct = db.Product.findByPk(productId, {
-      include: ["product_categories"],
-    });
-    let promProduct_category = db.Product.findAll();
-    Promise.all([promProduct, promProduct_category])
-      .then(([Product, allProduct_category]) => {
-        return res.render("productDetail", { Product, allProduct_category,  });
-      })
-      .catch((error) => res.send(error));
+    db.Product.findByPk(productId)
+    .then(producto => {
+      res.render("productDetail", {producto})
+    })
+    .catch((error) => res.send(error));
   },
 
   editProduct: (req, res) => {
